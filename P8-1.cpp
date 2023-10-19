39d36cdb74706354cafb5875059061b658e4c5ed
sslushie <sslushie22@gmail.com>
2023-10-19 11:48:44 -0400

    Completed countCharacter function

--- P8-1.cpp
+++ P8-1.cpp
@@ -1,1 +1,28 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+//function prototype
+int countCharacter(string str);
+
+int main() {
+	string input;
+	
+	while (true) {
+		cout << "Enter string or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q") break;
+
+		cout << " " << countCharacter(input)<< endl;
+	}
+	return 0;
+}
+
+//funcation declarations
+int countCharacter(string str) {
+	int n = 0;
+	for (char c : str) {
+		n++;
+	}
+	return n;
+}
